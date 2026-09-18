41a257786441c89900fe900ac424a1c7e4d26115
arhamajaz <arhamajaz18@gmail.com>
2026-09-18 23:18:06 +0530

    fix(testing): scope test matching to src/__tests__, ignore dist, and increase test timeout to 30s

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts', '**/*.test.ts', '**/*.test.js'],
+  testMatch: ['<rootDir>/src/__tests__/**/*.test.ts', '<rootDir>/src/__tests__/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testTimeout: 30000,
   verbose: true,
 };
+
